1df5a96a91ea97507ead5b0643a18c718d599f8e
Rahaf Alhabashi <rahafalhabashi@gmail.com>
2022-03-18 23:23:59 -0400

    commit tree 5becc27a14707484fbb33967f954fedaf3906777

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution in this file!
+const employee = {
+    name: "Rahaf Alhabashi",
+    streetAddress: {
+        street: "736 Bluelake Rd",
+        City: "College Park",
+        state: "Maryland",
+    }
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = {...obj};
+    newObj[key] = value;
+    return newObj;
+}
+
+const newName = updateEmployeeWithKeyAndValue(employee, "name", "Renad Alhabashi");
+
+newName.name;
+employee.name;
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value
+    return obj;
+}
+
+const permChangeCity = destructivelyUpdateEmployeeWithKeyAndValue(employee, "city", "Lanham");
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newObj = {...obj}
+    delete newObj[key];
+    return newObj;
+}
+
+const deletedState = destructivelyUpdateEmployeeWithKeyAndValue(employee, "state");
+
+function destructivelyDeleteFromEmployeeByKey(obj,key) {
+    delete obj[key];
+    return obj;
+}
+
+const destrDeletedState = destructivelyDeleteFromEmployeeByKey(employee, "state");
